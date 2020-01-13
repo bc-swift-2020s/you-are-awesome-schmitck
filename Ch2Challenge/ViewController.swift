59d1b44284a29dd020fcc205ea29f8afd7acdc1b
schmitck <schmitck@bc.edu>
2020-01-13 18:29:29 -0500

    Added a Color Change to the Labels.

--- Ch2Challenge/ViewController.swift
+++ Ch2Challenge/ViewController.swift
@@ -19,11 +19,13 @@ class ViewController: UIViewController {
     @IBAction func buttonOne(_ sender: UIButton) {
         print("Button one works!")
         messageLabel.text = "You are awesome!"
+        messageLabel.textColor = UIColor.black
     }
     
     @IBAction func buttonTwo(_ sender: UIButton) {
         print("Button two works!")
         messageLabel.text = "You are great!"
+        messageLabel.textColor = UIColor.blue
     }
     
 
