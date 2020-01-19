5d98497248ba73241741bc81d3b1f08d0649dd00
schmitck <schmitck@bc.edu>
2020-01-18 19:53:14 -0500

    Created a restrained app using the set restraints, not autosizing

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageLable.text = ""
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
