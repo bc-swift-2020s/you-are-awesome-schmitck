93887a3b88b22b7edcd018fb1abdf8eb57afb568
schmitck <schmitck@bc.edu>
2020-01-20 20:31:20 -0500

    finished ch 2.7: using conditional statements

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -21,8 +21,20 @@ class ViewController: UIViewController {
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        messageLable.text = "You Are Awesome!"
-        imageView.image = UIImage(named: "image0")
+        let awesomeMessage = "You Are Awesome!"
+        let greatMessage = "You Are Great!"
+        let bombMessage = "You Are Da Bomb!"
+        
+        if messageLable.text == awesomeMessage {
+            messageLable.text = greatMessage
+            imageView.image = UIImage(named: "image1")
+        } else if messageLable.text == greatMessage {
+            messageLable.text = bombMessage 
+            imageView.image = UIImage(named: "image2")
+        } else {
+            messageLable.text = awesomeMessage
+            imageView.image = UIImage(named: "image0")
+        }
     }
     
 }
