28741e33d5d7a0c7bf279b7fe74c5dcff2daead0
schmitck <schmitck@bc.edu>
2020-01-21 10:20:45 -0500

    Finished 2.9

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -16,38 +16,43 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     var imageNumber = 0
-    
+    var messageNumber = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         messageLable.text = ""
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
+        let messageArray = ["You are Awesome!", "You Rock!", "You are the best!", "You the man!"]
+        
+        messageLable.text = messageArray[messageNumber]
+        messageNumber += 1
+        if messageNumber == messageArray.count {
+            messageNumber = 0
+        }
         
         print(imageNumber)
-        let imageName = "image\(imageNumber)"
+        let imageName = "image\(imageNumber )"
         imageView.image = UIImage(named: imageName)
         imageNumber = imageNumber + 1
         if imageNumber == 10 {
             imageNumber = 0
         }
     
-        
-        
-        //        let awesomeMessage = "You Are Awesome!"
-        //        let greatMessage = "You Are Great!"
-        //        let bombMessage = "You Are Da Bomb!"
-        //
-        //        if messageLable.text == awesomeMessage {
-        //            messageLable.text = greatMessage
-        //            imageView.image = UIImage(named: "image1")
-        //        } else if messageLable.text == greatMessage {
-        //            messageLable.text = bombMessage
-        //            imageView.image = UIImage(named: "image2")
-        //        } else {
-        //            messageLable.text = awesomeMessage
-        //            imageView.image = UIImage(named: "image0")
-        //        }
+//        let awesomeMessage = "You Are Awesome!"
+//        let greatMessage = "You Are Great!"
+//        let bombMessage = "You Are Da Bomb!"
+//
+//        if messageLable.text == awesomeMessage {
+//            messageLable.text = greatMessage
+//            imageView.image = UIImage(named: "image1")
+//        } else if messageLable.text == greatMessage {
+//            messageLable.text = bombMessage
+//            imageView.image = UIImage(named: "image2")
+//        } else {
+//            messageLable.text = awesomeMessage
+//            imageView.image = UIImage(named: "image0")
+//        }
     }
     
 }
